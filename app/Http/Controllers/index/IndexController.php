a61a1bdbf831dda473a1bc7578207abf0e943349
zhushimao123 <152359342622@163.com>
2019-04-19 06:39:37 +0000

    shop

---
+++ app/Http/Controllers/index/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\index;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        echo 1111;
+    }
+}
