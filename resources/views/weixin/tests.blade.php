c64d2de4a37829c84bf71a5409ed7ae295fc542f
zhushimao123 <152359342622@163.com>
2019-04-23 12:01:10 +0000

    bug

--- resources/views/weixin/tests.blade.php
+++ resources/views/weixin/tests.blade.php
@@ -33,7 +33,7 @@
             $('#btn').click(function(){
                 //图像接口
                 wx.chooseImage({
-                    count: 1, // 默认9
+                    count: 2, // 默认9
                     sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
                     sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
                     success: function (res) {
