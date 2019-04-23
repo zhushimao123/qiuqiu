1c53a20a69d5ed877bdb804dc8b7b67e2db22db1
zhushimao123 <152359342622@163.com>
2019-04-23 09:41:15 +0000

    bug

--- resources/views/weixin/tests.blade.php
+++ resources/views/weixin/tests.blade.php
@@ -15,7 +15,7 @@
             debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
             appId: '{{$info['appId']}}', // 必填，公众号的唯一标识
             timestamp: '{{$info['timestamp']}}', // 必填，生成签名的时间戳
-            nonceStr: '{{$info['nonceStr']}}', // 必填，生成签名的随机串
+            nonceStr: '{{$info['noncestr']}}', // 必填，生成签名的随机串
             signature: '{{$info['signature']}}',// 必填，签名
             jsApiList: ['chooseImage'] // 必填，需要使用的JS接口列表
         });
