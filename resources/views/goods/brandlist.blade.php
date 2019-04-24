493ca6e3e9553325538620fa39c921243c09931d
zhushimao123 <152359342622@163.com>
2019-04-24 08:09:26 +0000

    接受微信服务器推送

--- resources/views/goods/brandlist.blade.php
+++ resources/views/goods/brandlist.blade.php
@@ -140,9 +140,10 @@ wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
     wx.onMenuShareTimeline({
     title: '球球', // 分享标题
     link: 'http://1809zhushimao.comcto.com/goodsinfo', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-    imgUrl: 'http://1809zhushimao.comcto.com/uploads/goodsimg/20190220/4f6e53dccdab7001b7a18359cedf8859.jpg', // 分享图标
+    imgUrl:'http://1809zhushimao.comcto.com/uploads/goodsimg/20190220/4f6e53dccdab7001b7a18359cedf8859.jpg', // 分享图标
     success: function () {
     // 用户点击了分享后执行的回调函数
-    alert(333);
+    alert(22);
     },
+})
 </script>
