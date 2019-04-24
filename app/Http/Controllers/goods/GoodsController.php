dd3797b308eb5d181c344cc971fdd1ebf379e19f
zhushimao123 <152359342622@163.com>
2019-04-24 10:49:28 +0000

    微信网页授权

--- app/Http/Controllers/goods/GoodsController.php
+++ app/Http/Controllers/goods/GoodsController.php
@@ -123,8 +123,9 @@ class GoodsController extends Controller
          */
         $url2= 'https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
         $user_info = json_decode(file_get_contents($url2),true);
-        // echo '<pre>';print_r($user_info);echo '</pre>';
+        echo '<pre>';print_r($user_info);echo '</pre>';
         $reult = DB::table('wx_user')->where(['openid'=>$user_info['openid']])->first()->toArray();
+        var_dump($reult);die;
         if($user_info['openid'] == $reult['openid']){
             echo  '欢迎'.$reult['nickname'].'回来';
         }else{
