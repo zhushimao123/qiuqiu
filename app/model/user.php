5da3123a6135567beee2ae06a134b26e9a858bd7
root <15235942622@163.com>
2019-04-28 09:27:24 +0800

    qiuqiu?

--- app/model/user.php
+++ app/model/user.php
@@ -4,8 +4,12 @@ namespace App\model;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Encore\Admin\Form;
+use Encore\Admin\Facades\Admin;
 class user extends Model
 {
     protected $table = 'wx_user';
     public $timestamps = false;
+
+  
 }
